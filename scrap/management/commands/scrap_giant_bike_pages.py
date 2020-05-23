16bae841bedb81b4587940c3b1dc5bcfb8438125
NicolleLouis <louisxnicolle@gmail.com>
2020-05-23 14:36:48 +0200

    Refacto

--- scrap/management/commands/scrap_giant_bike_pages.py
+++ scrap/management/commands/scrap_giant_bike_pages.py
@@ -14,33 +14,35 @@ class Command(BaseCommand):
         unscrapped_bikes = BikeRepository.get_unscrapped_bikes()
         for unscrapped_bike in unscrapped_bikes:
             request = requests.get(unscrapped_bike.page_url)
-            brand = unscrapped_bike.brand
-            year = unscrapped_bike.year
-            category = unscrapped_bike.category
             soup = BeautifulSoup(request.text, 'html.parser')
             self.remove_unwanted_div(soup)
 
             # Find if the product is alone or not
             if soup.find("div", {"id": "productsContainer"}) is not None:
-                print(unscrapped_bike.page_url)
-                tiles = soup.find_all("div", {"class": "tile"})
-                page_url = unscrapped_bike.page_url
-                unscrapped_bike.delete()
-                for tile in tiles:
-                    name = tile.find("h3").text
-                    bike_db, _created = BikeRepository.get_or_create_by_name_and_brand_and_year(
-                        name=name,
-                        brand=brand,
-                        year=year
-                    )
-                    bike_db.page_url = "https://www.giant-bicycles.com{}" \
-                        .format(tile.find("article").find("picture").find("a")["href"])
-                    bike_db.category = category
-                    bike_db.save()
+                self.manage_multiple_bike_pages(soup, unscrapped_bike)
             else:
                 ready_to_scrap_bikes += 1
 
         print("total number of bikes ready to be scrapped: {}".format(ready_to_scrap_bikes))
+
+    @staticmethod
+    def manage_multiple_bike_pages(soup, unscrapped_bike):
+        brand = unscrapped_bike.brand
+        year = unscrapped_bike.year
+        category = unscrapped_bike.category
+        tiles = soup.find_all("div", {"class": "tile"})
+        unscrapped_bike.delete()
+        for tile in tiles:
+            name = tile.find("h3").text
+            bike_db, _created = BikeRepository.get_or_create_by_name_and_brand_and_year(
+                name=name,
+                brand=brand,
+                year=year
+            )
+            bike_db.page_url = "https://www.giant-bicycles.com{}" \
+                .format(tile.find("article").find("picture").find("a")["href"])
+            bike_db.category = category
+            bike_db.save()
 
     @staticmethod
     def remove_unwanted_div(soup):
